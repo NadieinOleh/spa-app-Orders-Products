a14504cba4359cd75a7d230ae207fe835be12344
Nadiein Oleh <olegnadein1994@gmail.com>
2023-09-08 17:10:47 +0300

    added hashRoute

--- src/index.jsx
+++ src/index.jsx
@@ -5,18 +5,17 @@ import { store } from './app/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter }  from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
